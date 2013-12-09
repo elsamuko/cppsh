6f507956f6e7ffbb844287c10b805de2a58a7b1b
Samuel <elsamuko@gmail.com>
2013-12-09 20:32:03 +0100

    fix: sample compiles now with vs2012

--- test.cpp
+++ test.cpp
@@ -1,14 +1,20 @@
 #!/usr/bin/env cppsh
 
 #include <iostream>
+#ifndef _WIN32
 #include <initializer_list>
+#endif
 
 int main() {
     std::cout << "hello" << std::endl;
     
+#ifndef _WIN32
     for( auto i : { 1,2,3,4 } ) {
         std::cout << i << std::endl;
     }
+#else
+    std::cout << "sorry, no initializer lists for vs2012" << std::endl;
+#endif
     
     return 0;
 }
